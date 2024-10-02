d0c626cd4e2ff2fffceb337cfe64886063ba8105
MASA-10A\nisitani <masa24104@gmail.com>
2024-10-02 11:15:30 +0900

    2024/10/02 11:15

--- git.ps1
+++ git.ps1
@@ -1,6 +1,6 @@
 git add -A
 #git commit --allow-empty-message -m ""
-$date = Get-Date
+$date = Get-Date -Format g
 git commit -m $date
 $branch = git branch | Select-String '^\* ' | ForEach-Object { $_.Line -replace '^\* ', '' }
 Write-Output $branch
